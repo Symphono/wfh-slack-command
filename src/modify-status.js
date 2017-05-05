0dddad50239d1ac15e5819a17d768e52c616b48d
Tej Nagaraja <tejn@umich.edu>
2017-05-05 11:03:24 -0400

    -modify-status.js

---
+++ src/modify-status.js
@@ -0,0 +1,58 @@
+var slackApi = require('./slack-api');
+var emoji = require('node-emoji');
+
+var parseText = function (text) {
+
+	var unicodeCount = 0;
+	for (var i = 0; i < text.length; i += 1)
+	{
+		if (text.codePointAt(i) > 255)
+		{
+			unicodeCount += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return unicodeCount;
+}
+
+var getStatusEmojiAndText = function (text, unicodeCount) {
+	var statusEmoji = '';
+	var statusText = '';
+	if (text.codePointAt(0) > 255)
+	{
+		statusEmoji = text.substr(0, unicodeCount);
+		if (text.substr(unicodeCount, 1) === ' ')
+		{
+			statusText = text.substring(unicodeCount + 1);
+		}
+		else
+		{
+			statusText = text.substring(unicodeCount);
+		}
+		if (statusText === '')
+		{
+			statusText = 'Working from home';
+		}
+	}
+	else
+	{
+		// Status Emoji not specified at beginning of text, error
+	}
+	return { statusEmoji: statusEmoji, statusText: statusText };
+}
+
+module.exports = (text, token) => {
+	var encodedJSON = 'token='.concat(token, '&profile=', encodeURIComponent('{"status_text":"'.concat('', '","status_emoji":"', '', '"}')));
+	if (text !== 'clear' && text !== '')
+	{
+		var unicodeCount = parseText(text);
+		var statusEmoji = getStatusEmojiAndText(text, unicodeCount).statusEmoji;
+		var statusText = getStatusEmojiAndText(text, unicodeCount).statusText;
+		encodedJSON = 'token='.concat(token, '&profile=', encodeURIComponent('{"status_text":"'.concat(statusText, '","status_emoji":":', emoji.which(statusEmoji), ':"}')));
+	}
+	// Send encodedJSON back to slack (not done yet)
+	console.log(encodedJSON);
+};
