d84778c7fa1899c30c1fa043819c42edc30bc79a
Tej Nagaraja <tnagaraja@symphono.com>
2017-05-10 11:30:34 -0400

    Removed static instance in modify-status.js

--- src/modify-status.js
+++ src/modify-status.js
@@ -1,14 +1,20 @@
 var slackApi = require('./slack-api');
 var emoji = require('node-emoji');
 
-var status = {
-    status_text: 'Working from Home',
-    status_emoji: ':house_with_garden:'
+var getDefaultStatus = function () {
+	var status = {
+		status_text: 'Working from home',
+		status_emoji: ':house_with_garden:'
 	}
+		return status;
+}
 
-var defaultStatus = function () {
-		status.status_text = 'Working from home';
-		status.status_emoji = ':house_with_garden:';
+var getBlankStatus = function () {
+	var blankStatus = {
+		status_text: '',
+		status_emoji: ''
+	}
+	return blankStatus;
 }
 
 var getEmojiLength = function (text) {
@@ -28,33 +34,31 @@ var getEmojiLength = function (text) {
 	return emojiLength;
 }
 
-var setStatusEmojiAndText = function (text) {
+var buildStatusJSON = function (text) {
 	if (text === 'clear')
 	{
-		status.status_text = '';
-		status.status_emoji = '';
+		return getBlankStatus();
 	}
-	else
+
+	var status = getDefaultStatus();
+	var messageBegin = getEmojiLength(text);
+	if (text.codePointAt(0) > 255)
 	{
-		var messageBegin = getEmojiLength(text);
-		if (text.codePointAt(0) > 255)
+		status.status_emoji = ':'.concat(emoji.which(text.substr(0, messageBegin)), ':');
+		if (text.substr(messageBegin, 1) === ' ')
 		{
-			status.status_emoji = ':'.concat(emoji.which(text.substr(0, messageBegin)), ':');
-			if (text.substr(messageBegin, 1) === ' ')
-			{
-				messageBegin += 1;
-			}
-		}
-		if (text.substring(messageBegin).length > 0)
-		{
-			status.status_text = text.substring(messageBegin);
+			messageBegin += 1;
 		}
 	}
+	if (text.substring(messageBegin).length > 0)
+	{
+		status.status_text = text.substring(messageBegin);
+	}
+	return status;
 }
 
 module.exports = (text, token) => {
-	setStatusEmojiAndText(text);
+	var status = buildStatusJSON(text);
 	var encodedJSON = 'token='.concat(token, '&profile=', encodeURIComponent(JSON.stringify(status)));
-	defaultStatus();
 	return slackApi.sendResponse('slack.com/api/users.profile.set', encodedJSON);
 };
